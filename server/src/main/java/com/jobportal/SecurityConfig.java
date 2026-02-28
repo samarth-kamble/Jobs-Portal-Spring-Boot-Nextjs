29d5700ac8dd155181fb7795878618598f9e5968
Darshan patil <160645453+Dp0018@users.noreply.github.com>
2026-02-28 16:10:22 +0530

    Update SecurityConfig.java

--- server/src/main/java/com/jobportal/SecurityConfig.java
+++ server/src/main/java/com/jobportal/SecurityConfig.java
@@ -48,10 +48,10 @@ public class SecurityConfig {
     @Bean
     public CorsConfigurationSource corsConfigurationSource() {
         CorsConfiguration configuration = new CorsConfiguration();
-        configuration.setAllowedOrigins(Arrays.asList("http://localhost:3000", "http://localhost:3001","https://jobs-portal-spring-boot-nextjs.vercel.app")); // Ensure
-                                                                                                          // frontend
-                                                                                                          // URLs are
-                                                                                                          // allowed
+        configuration.setAllowedOrigins(Arrays.asList(
+                "http://localhost:3000",
+                "http://localhost:3001",
+                "https://jobs-portal-spring-boot-nextjs.vercel.app"));
         configuration.setAllowedMethods(Arrays.asList("GET", "POST", "PUT", "DELETE", "OPTIONS"));
         configuration.setAllowedHeaders(Arrays.asList("Authorization", "Content-Type", "Accept"));
         configuration.setAllowCredentials(true);
